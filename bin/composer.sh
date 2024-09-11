eba2c27d52e173e0166ffd29ebe1ad11603ec9eb
wagner.franca <wagner.franca@unesp.br>
2024-09-11 16:21:32 -0300

    makes wgn@wgnnote: bin$ ./composer.sh create-project --prefer-dist yiisoft/yii2-app-basic appname-yii2-basic creates the project in ./src-projects/www/html/ and at container creation the internet name will be a-common-network-name

--- bin/composer.sh
+++ bin/composer.sh
@@ -5,17 +5,21 @@
 # this script build composer container
 # mount your php app in this container
 # and runs composer install command inside the container
-
-composer_working_dir=$1
-composer_subcommand=$2
+composer_subcommand=$@
 container_name="phpdev-php8-composer"
 
 echo "###=> Detect the script directory independently of the execution directory"
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo $script_dir
+echo " ##### composer script_dir = $script_dir" 
 echo .
 
-echo "###=> dockerfile_path"
+# The path to the directory containing the PHP application
+# This will be mounted into the container in container /app dir
+app_home_dir="$script_dir/../src-projects/www/html"
+
+
+# providing the php container where we can runs composer command
+echo "###=> dockerfile_path (we need a php container to execute composer command)"
 dockerfile_path="$script_dir/../build-containers/php8-composer/Dockerfile"
 echo $dockerfile_path
 echo .
@@ -25,15 +29,11 @@ build_path="$script_dir/../build-containers/php8-composer"
 echo $build_path
 echo .
 
-echo "###=> container php_ini_dir"
+echo "###=> container php_ini_dir (our php container may need php.ini)"
 php_ini_dir="$script_dir/../build-containers/php8-composer/usr_local_etc_php"
 echo $php_ini_dir
 echo .
 
-echo "###=> container composer_working_dir"
-working_dir="$script_dir/.$composer_working_dir"
-echo $working_dir
-echo .
 
 # Build the Docker container from the Dockerfile
 docker build -t $container_name -f $dockerfile_path $build_path
@@ -45,6 +45,9 @@ docker build -t $container_name -f $dockerfile_path $build_path
 #Zend Engine v4.3.9, Copyright (c) Zend Technologies
 #docker run --rm -v "$working_dir:/app" -v "$php_ini_dir:/usr/local/etc/php" -w /app $container_name php -v
 
-docker run --rm -v "$working_dir:/app" -v "$php_ini_dir:/usr/local/etc/php" -w /app $container_name composer $composer_subcommand
+#show the concatenated composer command string that will be ruuning on
+echo " ##### "
+echo  docker run --rm -v "$app_home_dir:/app" -v "$php_ini_dir:/usr/local/etc/php" -w /app $container_name composer $composer_subcommand
 
-# Remove the container
+docker run --rm -v "$app_home_dir:/app" -v "$php_ini_dir:/usr/local/etc/php" -w /app $container_name composer $composer_subcommand
+
