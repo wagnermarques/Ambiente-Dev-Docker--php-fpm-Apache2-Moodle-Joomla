906e10f8a9dbbc3dcdd80474d879e9c0a6775154
wagner.franca <wagner.franca@unesp.br>
2024-09-11 14:50:51 -0300

    cosmetic changes

--- build-containers/mysql8-container/docker-entrypoint-initdb.d/_1_init.sql
+++ build-containers/mysql8-container/docker-entrypoint-initdb.d/_1_init.sql
@@ -1,6 +1,4 @@
 
 CREATE DATABASE moodle       DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE moodle_tests DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
---- https://docs.joomla.org/Creating_a_Database_for_Joomla!/pt-br
 CREATE DATABASE joomla          DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
